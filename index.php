9cd5edee02a6630c7e4acf0a93d9f92f2e1e3e1e
Tiagobte <tiago.pereira@mcq.local>
2024-06-26 10:47:10 -0300

    Alteração tela de login

--- index.php
+++ index.php
@@ -23,12 +23,39 @@ $username = $user['username'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Página Inicial</title>
 </head>
-<body>
-    <h2>Bem-vindo à Página Inicial</h2>
-    <p>Este é o conteúdo da página inicial após o login.</p>
-    <p>Usuário logado: <?php echo htmlspecialchars($username); ?></p>
-    <p><a href="logout.php">Sair</a></p>
+<body class="home">
+    <div class="container home">
+        <div class="welcome-message">
+            <h2>Bem-vindo à Página Inicial</h2>
+            <p><strong>Usuário logado:</strong> <?php echo htmlspecialchars($username); ?></p>
+        </div>
+        <div class="button-group-container">
+            <div class="button-group">
+                <a href="https://sintegre.ons.org.br/paginas/meu-perfil/meus-sistemas.aspx" target="_blank">ONS</a>
+                <a href="https://www.ccee.org.br/web/guest/precos/painel-precos" target="_blank">CCEE</a>
+                <a href="http://simepar.br/prognozweb/" target="_blank">Simepar</a>
+                <a href="https://stats.uptimerobot.com/2963AIKqz6" target="_blank">Uptime Robot</a>
+                <a href="https://pim.way2.com.br/GraficosHtml5" target="_blank">PIM Way2</a>
+                <a href="https://www.copel.com/mhbweb/paginas/bacia-iguacu.jsf" target="_blank">Copel</a>
+                <a href="http://servidor.ambitec.eng.br:8080/sistema/index.php?class=LoginForm&method=onReload&order=send_date&direction=asc" target="_blank">Ambitec</a>
+                <a href="https://hidro.tach.com.br/index.php" target="_blank">Hidro Tach</a>
+                <a href="https://selinc.com/pt/products/tables/ansi/" target="_blank">SEL Inc.</a>
+                <a href="https://webmail-seguro.com.br/" target="_blank">Acesso ao Webmail</a>
+                <a href="https://www.google.com.br/?hl=pt-BR" target="_blank">Acesso ao navegador</a>
+                <a href="https://web.skype.com/" target="_blank">Skype</a>
+                <a href="https://10.20.20.110/" target="_blank">IHM CAC</a>
+                <a href="https://www.copel.com/mhbweb/paginas/bacia-iguacu.jsf" target="_blank">COPEL Hidrologia</a>
+                <a href="http://10.20.120.5/" target="_blank">INTELBRAS</a>
+                <a href="logout.php" class="btn-logout">Sair</a>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> SeuSite. Todos os direitos reservados.</p>
+    </footer>
 </body>
 </html>
--- js/formulario.js
+++ js/formulario.js
@@ -11,30 +11,22 @@ function validateLoginForm() {
     return true;
 }
 
-// Função para validar formulário de registro
-function validateRegisterForm() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var password = document.getElementById('register-password').value;
-    var confirmPassword = document.getElementById('confirmPassword').value;
+// Função para validar formulário de login
+function validateLoginForm() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
-    if (name.trim() === '' || email.trim() === '' || phone.trim() === '' || password.trim() === '' || confirmPassword.trim() === '') {
-        document.getElementById('register-error').innerText = 'Por favor, preencha todos os campos.';
-        return false;
-    }
-
-    if (password !== confirmPassword) {
-        document.getElementById('register-error').innerText = 'As senhas digitadas não coincidem. Por favor, tente novamente.';
+    if (username.trim() === '' || password.trim() === '') {
+        document.getElementById('login-error').innerText = 'Por favor, preencha todos os campos.';
         return false;
     }
 
     return true;
 }
 
-// Adiciona listener para o evento submit do formulário de registro
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    if (!validateRegisterForm()) {
+// Adiciona listener para o evento submit do formulário de login
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    if (!validateLoginForm()) {
         event.preventDefault(); // Evita o envio do formulário se a validação falhar
     }
 });
